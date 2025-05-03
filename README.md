99b318b45e816b656957c8b281b84c5e3168a62a
Abhyuday <apsworks1212@gmail.com>
2025-05-04 02:07:54 +0530

    Update README and add STUN/TURN server images; modify usePeerConnection hook comments

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 # App Architecture
 ![Architecture](aseets/architectureImage.jpg)
+
+# STUN Servers
+![StunServers](aseets/StunArchitecture.png)
+
+# TURN Servers
+![TurnServers](aseets/TurnArchitecture.png)
--- hooks/usePeerConnection.tsx
+++ hooks/usePeerConnection.tsx
@@ -11,6 +11,12 @@ export const usePeerConnection = () => {
   const createPeer = useCallback(
     (stream: MediaStream, initiator: boolean) => {
       // Define ICE server configurations using Twilio's STUN and TURN servers
+      //  urls: [
+      //       "stun:stun.l.google.com:19302",
+      //       "stun:stun1.l.google.com:19302",
+      //       "stun:stun2.l.google.com:19302",
+      //       "stun:stun3.l.google.com:19302",
+      //     ],
       const stunServers: RTCIceServer[] = [
         {
           urls: "stun:global.stun.twilio.com:3478",
