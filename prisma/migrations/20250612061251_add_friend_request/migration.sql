c7c78b8cbc718451b8d747e4953fea16c34f6f6e
Abhyuday <apsworks1212@gmail.com>
2025-06-12 14:40:43 +0530

    Add FriendRequest and Friendship models with status enum; update User model for relationships

---
+++ prisma/migrations/20250612061251_add_friend_request/migration.sql
@@ -0,0 +1,48 @@
+-- CreateEnum
+CREATE TYPE "FriendRequestStatus" AS ENUM ('PENDING', 'ACCEPTED', 'REJECTED', 'CANCELLED');
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "userType" SET DEFAULT 'user';
+
+-- DropEnum
+DROP TYPE "UserType";
+
+-- CreateTable
+CREATE TABLE "FriendRequest" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "status" "FriendRequestStatus" NOT NULL DEFAULT 'PENDING',
+    "senderId" UUID NOT NULL,
+    "receiverId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "FriendRequest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Friendship" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "user1Id" UUID NOT NULL,
+    "user2Id" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Friendship_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FriendRequest_senderId_receiverId_key" ON "FriendRequest"("senderId", "receiverId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Friendship_user1Id_user2Id_key" ON "Friendship"("user1Id", "user2Id");
+
+-- AddForeignKey
+ALTER TABLE "FriendRequest" ADD CONSTRAINT "FriendRequest_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FriendRequest" ADD CONSTRAINT "FriendRequest_receiverId_fkey" FOREIGN KEY ("receiverId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Friendship" ADD CONSTRAINT "Friendship_user1Id_fkey" FOREIGN KEY ("user1Id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Friendship" ADD CONSTRAINT "Friendship_user2Id_fkey" FOREIGN KEY ("user2Id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
